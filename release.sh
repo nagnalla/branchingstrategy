b5e7437cefd428575bedfc6815d2a5fae30b6fd8
Samhitha Karnati <samhitha@Samhithas-MacBook-Air.local>
2018-12-15 23:31:45 -0500

    initial files to play around

---
+++ release.sh
@@ -0,0 +1,20 @@
+t branch
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+
+# PI.16.1,PI.16.2 etc.
+versionLabel=PI$1
+
+# establish branch and tag name variables
+devBranch=develop
+releaseBranch=release-$versionLabel
+ 
+# create the release branch from the -develop branch
+git checkout -b $releaseBranch $devBranch
+ 
+# commit version number increment
+git commit -am "Creating Release Branch with version number to $versionLabel"
+
+# merge release branch with the new version number back into develop
+git checkout $devBranch
+git merge --no-ff $releaseBranch
+
