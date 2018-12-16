871af51ffad991d5d25eb029261d79f6a7762f36
Samhitha Karnati <samhitha@Samhithas-MacBook-Air.local>
2018-12-15 23:46:04 -0500

    udpating release file

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
-t branch
+#!/bin/bash
+
+# current Git branch
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+git pull origin $branch
 
 # PI.16.1,PI.16.2 etc.
 versionLabel=PI-$1
@@ -7,7 +10,7 @@ versionLabel=PI-$1
 # establish branch and tag name variables
 devBranch=develop
 releaseBranch=release-$versionLabel
- 
+
 # create the release branch from the -develop branch
 git checkout -b $releaseBranch $devBranch
  
@@ -17,7 +20,7 @@ git commit -am "Creating Release Branch with version number to $versionLabel"
 #push new branch to remote origin
 git push origin $releaseBranch
 
-# merge release branch with the new version number back into develop
-git checkout $devBranch
-git merge --no-ff $releaseBranch
+# merge release branch with the new version number back into develop - Not used yet
+#git checkout $devBranch
+#git merge --no-ff $releaseBranch
 
